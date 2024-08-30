41ca7715fa6c856ffffd92af312c812343ed8b6f
James Green <jkg@earth.li>
2024-08-30 15:41:38 +0100

    Tidy up POD in new Member.pm file - will be generated on build/release

--- lib/Telegram/Bot/Object/Member.pm
+++ lib/Telegram/Bot/Object/Member.pm
@@ -1,7 +1,13 @@
 package Telegram::Bot::Object::Member;
-$Telegram::Bot::Object::Member::VERSION = '0.026';
+
 # ABSTRACT: The base class for Telegram message 'Member' type.
 
+=head1 DESCRIPTION
+
+See L<https://core.telegram.org/bots/api#chatmember> for details of the
+attributes available for L<Telegram::Bot::Object::Member> objects.
+
+=cut
 
 use Mojo::Base 'Telegram::Bot::Object::Base';
 
@@ -37,53 +43,3 @@ sub fields {
 }
 
 1;
-
-__END__
-
-=pod
-
-=encoding UTF-8
-
-=head1 NAME
-
-Telegram::Bot::Object::Member - The base class for Telegram message 'Member' type.
-
-=head1 VERSION
-
-version 0.025
-
-=head1 DESCRIPTION
-
-See L<https://core.telegram.org/bots/api#user> for details of the
-attributes available for L<Telegram::Bot::Object::Member> objects.
-
-=head1 AUTHORS
-
-=over 4
-
-=item *
-
-Justin Hawkins <justin@eatmorecode.com>
-
-=item *
-
-James Green <jkg@earth.li>
-
-=item *
-
-Julien Fiegehenn <simbabque@cpan.org>
-
-=item *
-
-Albert Cester <albert.cester@web.de>
-
-=back
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2024 by James Green.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
-=cut
